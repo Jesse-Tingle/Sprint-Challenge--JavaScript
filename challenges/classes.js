214676cc5cc13afdfbdb7746498d7df26a3863b6
Jesse-Tingle <jtingle0@gmail.com>
2019-07-14 21:31:48 -0400

    updated constructor class name to create a unique name and updated object variable name line 23 to be a unique name - got rid of error message

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker {
+class CuboidMakerClass {
     constructor(attributes){
         this.length = attributes.length;
         this.width = attributes.width;
@@ -20,7 +20,7 @@ class CuboidMaker {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboid = new CuboidMaker ({
+const cuboidClass = new CuboidMakerClass ({
   length: 4,
   width: 5,
   height: 5,
@@ -29,7 +29,7 @@ const cuboid = new CuboidMaker ({
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
- console.log(cuboid.volume()); // 100
- console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboidClass.volume()); // 100
+ console.log(cuboidClass.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
